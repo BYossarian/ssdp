ff2b8e8ef4806d6a6ee896840621ce78d79eae26
Ben Jackson <benjackson@Bens-iMac.local>
2015-04-17 11:28:16 +0100

    proper async for client creation

--- demo/demo-client.js
+++ demo/demo-client.js
@@ -3,7 +3,15 @@
 
 var ssdp = require('../index.js');
 
-var client = ssdp.createClient(1901);
+var client = ssdp.createClient(1901, function(err, client) {
+
+    if (err) {
+        return console.log(err);
+    }
+
+    client.search('urn:dial-multiscreen-org:service:dial:1');
+
+});
 
 client.on('response', function(headers) {
 
@@ -11,5 +19,3 @@ client.on('response', function(headers) {
     console.log('\n');
 
 });
-
-client.search('urn:dial-multiscreen-org:service:dial:1');
