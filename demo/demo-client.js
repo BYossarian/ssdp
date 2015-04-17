b47e19f1bea0023197903476327e74d10b32cf9f
Ben <benpeterjackson@gmail.com>
2015-04-17 23:15:21 +0100

    clean up async

--- demo/demo-client.js
+++ demo/demo-client.js
@@ -3,19 +3,19 @@
 
 var ssdp = require('../index.js');
 
-var client = ssdp.createClient(1901, function(err, client) {
+ssdp.createClient(1901, function(err, client) {
 
     if (err) {
         return console.log(err);
     }
 
+    client.on('response', function(headers) {
+
+        console.log(headers);
+        console.log('\n');
+
+    });
+
     client.search('urn:dial-multiscreen-org:service:dial:1');
-
-});
-
-client.on('response', function(headers) {
-
-    console.log(headers);
-    console.log('\n');
 
 });
--- demo/demo-device.js
+++ demo/demo-device.js
@@ -3,7 +3,7 @@
 
 var ssdp = require('../index.js');
 
-var device = ssdp.createDevice({
+ssdp.createDevice({
     uuid: '2fac1234-31f8-11b4-a222-08002b34c003',
     serviceList: ['upnp:rootdevice', 'urn:dial-multiscreen-org:device:dial:1', 'urn:dial-multiscreen-org:service:dial:1'],
     location: 'http://url.to.your/device.description.xml'
