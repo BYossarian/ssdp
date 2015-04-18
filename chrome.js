d34aa3b453ec49a437242a9166c29474f11a363c
Ben <benpeterjackson@gmail.com>
2015-04-18 17:44:52 +0100

    huge refactor

--- chrome.js
+++ chrome.js
@@ -1,8 +1,8 @@
 // module interface for chrome os
 
-var ssdp = require('./lib/ssdp-chrome.js');
+var udp = require('./lib/udp/udp-chrome.js');
 
 module.exports = {
-    createClient: require('./lib/client.js')(ssdp),
-    createDevice: require('./lib/device.js')(ssdp)
+    createClient: require('./lib/client.js')(udp),
+    createDevice: require('./lib/device.js')(udp)
 };
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // module interface for node
 
-var ssdp = require('./lib/ssdp-node.js');
+var udp = require('./lib/udp/udp-node.js');
 
 module.exports = {
-    createClient: require('./lib/client.js')(ssdp),
-    createDevice: require('./lib/device.js')(ssdp)
+    createClient: require('./lib/client.js')(udp),
+    createDevice: require('./lib/device.js')(udp)
 };
